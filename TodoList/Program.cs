164d93e96d713aca84ea113f01f1794366810072
Vlad Tokmakov <vlad.tokmakov.07@mail.ru>
2025-09-27 00:42:07 +0300

    Добавил первоночальное сообщение

---
+++ TodoList/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Todolist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Работу выполнили Токмаков и Неофидис");
+        }
+    }
+}
