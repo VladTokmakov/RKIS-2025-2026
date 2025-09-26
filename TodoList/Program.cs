c18c7e56cfb148284c3ffe4e84ff43c90a2e2430
Vlad Tokmakov <vlad.tokmakov.07@mail.ru>
2025-09-27 00:56:06 +0300

    Получил введенные пользователем значения

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Работу выполнили Токмаков и Неофидис");
+
+            Console.Write("Введите ваше имя: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Введите вашу фамилию: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Введите ваш год рождения: ");
+            string yearBirthString = Console.ReadLine();
         }
     }
 }
