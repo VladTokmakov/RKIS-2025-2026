197723740f2a7bb9e17657f045fb0ec5fbefc878
Vlad Tokmakov <vlad.tokmakov.07@mail.ru>
2025-10-04 00:03:25 +0300

    Lesson 3: Added a command 'profile' and 'adduser', Task 4

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -2,25 +2,13 @@
 {
     class Program
     {
+        private static Person user;
+ 
         static void Main(string[] args)
         {
             Console.WriteLine("Работу выполнили Токмаков и Неофидис");
 
-            /*Console.Write("Введите ваше имя: ");
-            string firstName = Console.ReadLine();
-
-            Console.Write("Введите вашу фамилию: ");
-            string lastName = Console.ReadLine();
-
-            Console.Write("Введите ваш год рождения: ");
-            string yearBirthString = Console.ReadLine();
-
-            int yearBirth = int.Parse(yearBirthString);
-
-            int age = DateTime.Now.Year - yearBirth;
-
-            Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, возраст – {age}");
-            */
+            //GetDataUser();
 
             string[] todos = new string[10];
 
@@ -36,6 +24,11 @@
                         break;
 
                     case "profile":
+                        Profile();
+                        break;
+
+                    case "adduser":
+                        GetDataUser();
                         break;
 
                     case "add":
@@ -50,7 +43,25 @@
 
             }
         }
+        static void GetDataUser()
+        {
+            Console.Write("Введите ваше имя: ");
+            string firstName = Console.ReadLine();
 
+            Console.Write("Введите вашу фамилию: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Введите ваш год рождения: ");
+            int yearBirth = int.Parse(Console.ReadLine());
+
+            int age = DateTime.Now.Year - yearBirth;
+
+            user = new Person(firstName, lastName, yearBirth, age);
+
+            Console.WriteLine($"Добавлен пользователь {firstName} {lastName}, Год рождения: {yearBirth}, возраст – {age}");
+            Console.WriteLine();
+            //Console.ReadLine();
+        }
         static void Help()
         {
             Console.WriteLine("Доступные команды:");
@@ -60,6 +71,38 @@
             Console.WriteLine("view    - выводит все задачи из массива");
             Console.WriteLine("exit    - завершает программу");
             Console.ReadLine();
+        }
+
+        static void Profile()
+        {
+            if (user != null)
+            {
+                Console.WriteLine($"Пользователь: {user.FirstName} {user.LastName}");
+                Console.WriteLine($"Год рождения: {user.YearBirth}");
+                Console.WriteLine($"Возраст: {user.Age}");
+            }
+            else
+            {
+                Console.WriteLine("Данные пользователя не найдены");
+            }
+            Console.ReadLine();
+        }
+
+    }
+
+    class Person
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public int YearBirth { get; }
+        public int Age { get; }
+
+        public Person(string firstName, string lastName, int yearBirth, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            YearBirth = yearBirth;
+            Age = age;
         }
 
     }
