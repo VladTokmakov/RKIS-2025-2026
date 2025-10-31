ea5afd536a6ba55ccabfa8738bdcb15a24874392
VladTokmakov <vlad.tokmakov.07@mail.ru>
2025-11-01 02:46:03 +0300

    Lesson 7: Added class 'ProfileCommand', Task 3

---
+++ TodoList/ProfileCommand.cs
@@ -0,0 +1,24 @@
+namespace Todolist
+{
+    public class ProfileCommand : ICommand
+    {
+        public Profile UserProfile { get; private set; }
+
+        public ProfileCommand(Profile userProfile)
+        {
+            UserProfile = userProfile;
+        }
+
+        public void Execute()
+        {
+            if (UserProfile != null)
+            {
+                Console.WriteLine(UserProfile.GetInfo());
+            }
+            else
+            {
+                Console.WriteLine("Данные пользователя не найдены");
+            }
+        }
+    }
+}
