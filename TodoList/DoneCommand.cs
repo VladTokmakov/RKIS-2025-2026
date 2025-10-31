a668e09c133573651473cd4b77f8c7cf770004f9
VladTokmakov <vlad.tokmakov.07@mail.ru>
2025-11-01 02:19:07 +0300

    Lesson 7: Added class 'DoneCommand', Task 3

---
+++ TodoList/DoneCommand.cs
@@ -0,0 +1,28 @@
+using System;
+namespace Todolist
+{
+    public class DoneCommand : ICommand
+    {
+        public int TaskNumber { get; private set; }
+        public Todolist TodoList { get; private set; }
+
+        public DoneCommand(Todolist todoList, int taskNumber)
+        {
+            TodoList = todoList;
+            TaskNumber = taskNumber;
+        }
+
+        public void Execute()
+        {
+            if (TaskNumber <= 0 || TaskNumber > TodoList.GetCount())
+            {
+                Console.WriteLine($"Неверный номер задачи. Должен быть от 1 до {TodoList.GetCount()}");
+                return;
+            }
+
+            TodoItem item = TodoList.GetItem(TaskNumber - 1);
+            item.MarkDone();
+            Console.WriteLine($"Задача №{TaskNumber} отмечена как выполненная");
+        }
+    }
+}
