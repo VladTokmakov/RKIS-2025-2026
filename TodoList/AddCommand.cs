908db06ac17902eb06d6da035435dd319b89fec6
VladTokmakov <vlad.tokmakov.07@mail.ru>
2025-11-01 00:33:08 +0300

    Lesson 7: Added class 'AddCommand', Task 3

---
+++ TodoList/AddCommand.cs
@@ -0,0 +1,50 @@
+using System;
+namespace Todolist
+{
+    public class AddCommand : ICommand
+    {
+        public bool IsMultiline { get; private set; }
+        public string TaskText { get; private set; }
+        public Todolist TodoList { get; private set; }
+
+        public AddCommand(Todolist todoList, string taskText, bool isMultiline = false)
+        {
+            TodoList = todoList;
+            TaskText = taskText;
+            IsMultiline = isMultiline;
+        }
+
+        public void Execute()
+        {
+            if (IsMultiline)
+            {
+                Console.WriteLine("Многострочный режим. Введите задачи (для завершения введите '!end'):");
+                string multilineText = "";
+
+                Console.Write("> ");
+                string line = Console.ReadLine();
+                while (line.ToLower() != "!end")
+                {
+                    Console.Write("> ");
+                    multilineText += line + "\n";
+                    line = Console.ReadLine();
+                }
+
+                TodoItem newItem = new TodoItem(multilineText.Trim());
+                TodoList.Add(newItem);
+                Console.WriteLine($"Добавлена задача №{TodoList.GetCount()}: {multilineText.Trim()}");
+            }
+            else
+            {
+                if (TaskText.StartsWith("\"") && TaskText.EndsWith("\""))
+                {
+                    TaskText = TaskText.Substring(1, TaskText.Length - 2);
+                }
+
+                TodoItem newItem = new TodoItem(TaskText);
+                TodoList.Add(newItem);
+                Console.WriteLine($"Добавлена задача №{TodoList.GetCount()}: {TaskText}");
+            }
+        }
+    }
+}
