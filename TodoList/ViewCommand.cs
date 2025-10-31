ae85ee3fe27709d1e3ac2417b6fde281329389c1
VladTokmakov <vlad.tokmakov.07@mail.ru>
2025-11-01 01:02:04 +0300

    Lesson 7: Added class 'ViewCommand', Task 3

---
+++ TodoList/ViewCommand.cs
@@ -0,0 +1,36 @@
+using System;
+namespace Todolist
+{
+    public class ViewCommand : ICommand
+    {
+        public bool ShowIndex { get; private set; }
+        public bool ShowStatus { get; private set; }
+        public bool ShowDate { get; private set; }
+        public bool ShowAll { get; private set; }
+        public Todolist TodoList { get; private set; }
+
+        public ViewCommand(Todolist todoList, bool showIndex = false, bool showStatus = false, bool showDate = false, bool showAll = false)
+        {
+            TodoList = todoList;
+            ShowIndex = showIndex;
+            ShowStatus = showStatus;
+            ShowDate = showDate;
+            ShowAll = showAll;
+        }
+
+        public void Execute()
+        {
+            if (TodoList.GetCount() == 0)
+            {
+                Console.WriteLine("Задачи отсутствуют");
+                return;
+            }
+
+            bool showIndex = ShowIndex || ShowAll;
+            bool showStatus = ShowStatus || ShowAll;
+            bool showDate = ShowDate || ShowAll;
+
+            TodoList.View(showIndex, showStatus, showDate);
+        }
+    }
+}
