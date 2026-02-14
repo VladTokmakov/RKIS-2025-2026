577e3f75f9e205d809823e41c08745a4414063b5
VladTokmakov <vlad.tokmakov.07@mail.ru>
2026-02-14 06:17:27 +0300

    Lesson 1: Added a command 'searsh' and flags

--- TodoList/Commands/UpdateCommand.cs
+++ TodoList/Commands/UpdateCommand.cs
@@ -65,6 +65,8 @@ namespace Todolist
                 {
                     TodoList.GetItem(index).UpdateText(_oldText);
                     Console.WriteLine($"Текст задачи №{TaskNumber} возвращен к предыдущему значению.");
+
+                    
                     if (!string.IsNullOrEmpty(TodoFilePath)) 
                         FileManager.SaveTodos(TodoList, TodoFilePath);
                 }
